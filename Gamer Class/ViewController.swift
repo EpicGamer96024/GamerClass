9124432caca45fcae4daae086ed03edb7f24b44d
Logan <lchristmas21@hollandhall.org>
2019-09-20 14:32:58 -0500

    Initial Commit

---
+++ Gamer Class/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Gamer Class
+//
+//  Created by Logan Christmas on 9/20/19.
+//  Copyright © 2019 Logan Christmas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
